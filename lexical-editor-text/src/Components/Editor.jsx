da19fc49527098862d7036e1763c39b206be83e8
Kefflen Moreno Ramos <55823300+kefflen@users.noreply.github.com>
2022-05-21 19:36:34 -0300

    Entrada de texto

--- lexical-editor-text/src/Components/Editor.jsx
+++ lexical-editor-text/src/Components/Editor.jsx
@@ -1,39 +1,40 @@
-import React from 'react';
-import { $getRoot, $getSelection } from 'lexical';
-import LexicalComposer from '@lexical/react/LexicalComposer';
-import LexicalPlainTextPlugin from '@lexical/react/LexicalPlainTextPlugin';
-import LexicalRichTextPlugin from '@lexical/react/LexicalRichTextPlugin';
-import LexicalContentEditable from '@lexical/react/LexicalContentEditable';
-import LexicalOnChangePlugin from '@lexical/react/LexicalOnChangePlugin';
-import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
+import React from 'react'
+import { $getRoot, $getSelection } from 'lexical'
+import LexicalComposer from '@lexical/react/LexicalComposer'
+import LexicalRichTextPlugin from '@lexical/react/LexicalRichTextPlugin'
+import LexicalContentEditable from '@lexical/react/LexicalContentEditable'
+import LexicalOnChangePlugin from '@lexical/react/LexicalOnChangePlugin'
+import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin'
+import { Container } from './styled'
 
 function onChange(state) {
   state.read(() => {
-    const root = $getRoot();
-    const selection = $getSelection();
-    console.log(selection);
-  });
+    const root = $getRoot()
+    console.log(root)
+    const selection = $getSelection()
+    console.log(selection)
+  })
 }
 
 export const Editor = () => {
   return (
-    <div className='bg-white relative rounded-sm'>
+    <Container>
       <LexicalComposer
         initialConfig={{
           theme: {
             paragraph: 'mb-1',
           },
           onError(error) {
-            throw error;
+            throw error
           },
         }}
       >
         <LexicalRichTextPlugin
           contentEditable={
-            <LexicalContentEditable className="min-h-[450px] outline-none py-[15px] px-2.5 resize-none overflow-hidden text-ellipsis" />
+            <LexicalContentEditable className="edit-block" />
           }
           placeholder={
-            <div className="absolute top-[15px] left-[10px] pointer-events-none select-none">
+            <div className="placehoder">
               Now write something brilliant...
             </div>
           }
@@ -41,6 +42,6 @@ export const Editor = () => {
         <LexicalOnChangePlugin onChange={onChange} />
         <HistoryPlugin />
       </LexicalComposer>
-    </div>
+    </Container>
   )
 }
---
+++ lexical-editor-text/src/Components/styled.jsx
@@ -0,0 +1,30 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+  border-radius: 5px;
+  box-shadow: 1px 0.5px 0.3em black;
+  margin: 10px;
+
+  .edit-block {
+    outline: none;
+    height: 450px;
+    padding: 15px;
+    overflow: hidden;
+    overflow-y: scroll;
+    text-overflow: ellipsis;
+    resize: none;
+
+    ::-webkit-scrollbar {
+      display: none;
+    }
+
+  } 
+  .placehoder {
+    position: absolute;
+    top: 15px;
+    left: 15px;
+    pointer-events: none;
+    user-select: none;
+    color: gray;
+  }
+`
